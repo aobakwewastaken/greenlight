72a10fb66a989760fd2822e08c56354456706544
OkayAobakwe <okayaobakwe@gmail.com>
2023-01-04 19:49:10 +0200

    wip(movies): adds dummy movies route, for creating and getting movies

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	return router
 }
