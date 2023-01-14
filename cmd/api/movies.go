07b8fe444916a206cc50f8efc1532e71052c21c2
OkayAobakwe <okayaobakwe@gmail.com>
2023-01-14 23:53:33 +0200

    feat(movies): creates movie struct for json encoding

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/aobakwewastaken/greenlight/internal/data"
 )
 
 func (app *Application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +18,20 @@ func (app *Application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance"},
+		Version:   1,
+	}
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
 	fmt.Fprintf(w, "show the details of the movie %d\n", id)
 }
