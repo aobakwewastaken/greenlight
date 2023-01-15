112af1a5b74f06a11e7e584bcac88c7c8092087e
OkayAobakwe <okayaobakwe@gmail.com>
2023-01-15 13:28:15 +0200

    chore(movies): handle 404 on movies with custom error

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -16,7 +16,7 @@ func (app *Application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
