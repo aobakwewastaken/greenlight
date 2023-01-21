d567930c4d4415e87c5888ae6b04fcae3a7d1d1a
aobakwewastaken <okayaobakwe@gmail.com>
2023-01-21 23:30:33 +0200

    feat(create movies): validate title and year

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/aobakwewastaken/greenlight/internal/data"
+	"github.com/aobakwewastaken/greenlight/internal/validator"
 )
 
 func (app *Application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +23,13 @@ func (app *Application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.badRequestResponse(w, r, err)
 		return
 	}
+	v := validator.New()
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
