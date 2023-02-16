f4fc09f7add20c8cf0936be77641d474097f06c8
aobakwewastaken <okayaobakwe@gmail.com>
2023-02-16 22:42:30 +0200

    chore(movies): setup the get query for searching a movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/aobakwewastaken/greenlight/internal/data"
 	"github.com/aobakwewastaken/greenlight/internal/validator"
@@ -55,13 +55,15 @@ func (app *Application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Casablanca",
-		Runtime:   102,
-		Genres:    []string{"drama", "romance"},
-		Version:   1,
+	movie, err := app.models.Movie.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
 	}
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
