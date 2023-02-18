1768df050e8efa8ddaf63376907796a782a04260
aobakwewastaken <okayaobakwe@gmail.com>
2023-02-18 20:32:28 +0200

    chore(update movie): adds update logic

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -65,7 +65,19 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 }
 
 func (m MovieModel) Update(movie *Movie) error{
-	return nil
+	query := `
+	UPDATE movie
+	SET title = $1,year = $2,runtime = $3,genres = $4,version = version + 1
+	WHERE id = $5
+	`
+	args := []interface{}{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 func (m MovieModel) Delete(id int64) error{
